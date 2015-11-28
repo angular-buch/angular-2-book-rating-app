3e0db7f259999e4820707085edbbc2e8dd7a5236
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-11-28 21:51:19 +0100

    allowing js-files and dist files of systemjs, bootstrap and angular2 for gh-pages

---
+++ src/app/models/book.js
@@ -0,0 +1,11 @@
+var Book = (function () {
+    function Book(title, comment) {
+        this.rating = 0;
+        this.title = title;
+        this.comment = comment;
+    }
+    return Book;
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Book;
+//# sourceMappingURL=book.js.map
