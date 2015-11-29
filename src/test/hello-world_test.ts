c832a07b41d5bf5096bb956f38071b9f7988a2f8
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-11-29 20:55:47 +0100

    test: Book component rateUp/rateDown increases rating, should call the rated event

--- src/test/hello-world_test.ts
+++ src/test/hello-world_test.ts
@@ -1,4 +1,4 @@
-import { it, describe, expect, inject, beforeEachProviders, } from 'angular2/testing';
+import { it, describe, expect, inject, beforeEachProviders } from 'angular2/testing';
 
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
