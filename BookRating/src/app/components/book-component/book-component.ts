35be3abe2073b14163923becb3e7177fae8c8da9
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-03-09 17:32:21 +0100

    BookComponent: now with-rating

--- BookRating/src/app/components/book-component/book-component.ts
+++ BookRating/src/app/components/book-component/book-component.ts
@@ -10,8 +10,14 @@ import {Book} from '../../models/book';
       </div>
       <h2>{{ book.title }} <small>Stars {{ book.rating }}</small></h2>
       <p>{{ book.comment }}</p>
+
+      <button (click)="rateUp()" class="btn btn-default glyphicon glyphicon-thumbs-up"></button>
+      <button (click)="rateDown()" class="btn btn-default glyphicon glyphicon-thumbs-down"></button>
     </div>`
 })
 export class BookComponent {
   @Input() book: Book;
+
+  rateUp() { this.book.rating++; }
+  rateDown() { this.book.rating--; }
 }
