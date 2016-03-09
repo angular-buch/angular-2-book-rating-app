032cedecdc7ea82d71244ae255ea3e0b6cdd1654
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-03-09 17:23:49 +0100

    Dashboard class: dashboard has form and shows a list of books

--- BookRating/src/app/components/dashboard/dashboard.ts
+++ BookRating/src/app/components/dashboard/dashboard.ts
@@ -6,13 +6,35 @@ import {BookComponent} from '../book-component/book-component';
   selector: 'dashboard',
   directives: [BookComponent],
   template: `
-    <h1>Buch</h1>
-    <book [book]="book"></book>`
+     <div class="form">
+       <div class="form-group">
+         <div><label for="title">Title</label></div>
+         <div><input class="form-control" name="title" #title></div>
+       </div>
+       <div class="form-group">
+         <div><label for="link">Comment</label></div>
+         <div><textarea class="form-control" name="comment" #comment></textarea></div>
+       </div>
+       <div class="form-group">
+        <button (click)="add(title, comment)" class="btn btn-danger">Submit</button>
+       </div>
+     </div>
+
+     <hr>
+     <book *ngFor="#book of books" [book]="book"></book>`
 })
 export class Dashboard {
-  book: Book;
+  books: Book[];
 
   constructor() {
-    this.book = new Book('Angular 2', 'Eine praktische Einführung');
+    this.books = [new Book('Angular 2', 'Eine praktische Einführung')];
+  }
+
+  add(title, comment) {
+    var newBook = new Book(title.value, comment.value);
+    this.books.push(newBook);
+
+    title.value = '';
+    comment.value = '';
   }
 }
