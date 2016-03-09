fd46f6bbdeee5360374643ae705b6d319a1c9337
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-03-09 13:42:23 +0100

    $ ng generate component BookRating

---
+++ BookRating/src/app/components/book-rating/book-rating.spec.ts
@@ -0,0 +1,27 @@
+import {
+  it,
+  iit,
+  describe,
+  ddescribe,
+  expect,
+  inject,
+  injectAsync,
+  TestComponentBuilder,
+  beforeEachProviders
+} from 'angular2/testing';
+import {provide} from 'angular2/core';
+import {BookRating} from './book-rating';
+
+
+describe('BookRating Component', () => {
+
+  beforeEachProviders((): any[] => []);
+
+
+  it('should ...', injectAsync([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+    return tcb.createAsync(BookRating).then((fixture) => {
+      fixture.detectChanges();
+    });
+  }));
+
+});
---
+++ BookRating/src/app/components/book-rating/book-rating.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'book-rating',
+  templateUrl: 'app/components/book-rating/book-rating.html',
+  styleUrls: ['app/components/book-rating/book-rating.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class BookRating {
+
+  constructor() {}
+
+}
