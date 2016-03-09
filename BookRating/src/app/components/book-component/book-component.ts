b87b622936dd194ae1c91e28554717f18a44a0fa
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-03-09 15:54:01 +0100

    $ ng generate component BookComponent

---
+++ BookRating/src/app/components/book-component/book-component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'book-component',
+  templateUrl: 'app/components/book-component/book-component.html',
+  styleUrls: ['app/components/book-component/book-component.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class BookComponent {
+
+  constructor() {}
+
+}
---
+++ BookRating/src/models/book.ts
@@ -0,0 +1,6 @@
+// book.ts
+export class Book {
+  rating: number = 0;
+  
+  constructor(public title: string, public comment: string) { }
+}
