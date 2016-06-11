6e7f5e1705dbcce6d8679a9e8be41714dabb4bb7
Gregor Woiwode <gw@co-it.eu>
2016-06-11 17:35:37 +0200

    Removes obsolete test

--- book-rating/src/app/app.component.spec.ts
+++ book-rating/src/app/app.component.spec.ts
@@ -14,9 +14,4 @@ describe('App: BookRating', () => {
       inject([AppComponent], (app: AppComponent) => {
     expect(app).toBeTruthy();
   }));
-
-  it('should have as title \'app works!\'',
-      inject([AppComponent], (app: AppComponent) => {
-    expect(app.title).toEqual('app works!');
-  }));
 });
