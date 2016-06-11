9f2a2d13e9b80c8ddc5d0f937d765d4df0a2bf70
Gregor Woiwode <gw@co-it.eu>
2016-06-11 17:37:49 +0200

    Tests if the book is initialized with a rating of 0

--- book-rating/src/app/shared/book.spec.ts
+++ book-rating/src/app/shared/book.spec.ts
@@ -9,6 +9,10 @@ import {Book} from './book';
 
 describe('Book', () => {
   it('should create an instance', () => {
-    expect(new Book()).toBeTruthy();
+    expect(new Book('a', 'b', 1)).toBeTruthy();
+  });
+
+  it('has a rating of 0', () => {
+    expect(new Book('a', 'b').rating).toEqual(0);
   });
 });
