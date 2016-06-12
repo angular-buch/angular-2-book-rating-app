2269da5c29b37d246e8606c1441c732ccd28cd1b
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-06-12 15:54:05 +0200

    forms example /  Dashboard component (Event binding)

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -12,11 +12,15 @@ export class DashboardComponent implements OnInit {
 
   books: Book[];
 
-  constructor(){}
-
   ngOnInit() {
     this.books = [
       new Book('Angular 2', 'Einstieg in die komponentenbasierte Entwicklung'),
-      new Book('Bericht DWX 2016', 'Das haben wir erlebt', 5)];
+      new Book('Bericht DWX 2016', 'Das haben wir erlebt')];
+  }
+
+  add(title, comment) {
+    var newBook = new Book(title.value, comment.value);
+    this.books.push(newBook);
+    title.value = comment.value = '';
   }
 }
