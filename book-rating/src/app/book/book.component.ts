5e575059e703e559cf8717e4181e0b2000ebad2c
Gregor Woiwode <gw@co-it.eu>
2016-06-21 09:25:29 +0200

    Adds component book

---
+++ book-rating/src/app/book/book.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Book } from '../shared';
+
+@Component({
+  moduleId: module.id,
+  selector: 'br-book',
+  templateUrl: 'book.component.html',
+  styleUrls: ['book.component.css']
+})
+export class BookComponent implements OnInit {
+  @Input() book: Book;
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
