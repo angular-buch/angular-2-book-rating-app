f08e8355a868e8b6d1721c692d4ad91491888af4
Gregor Woiwode <gw@co-it.eu>
2016-06-21 08:53:44 +0200

    Adds component dashboard

---
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'br-dashboard',
+  templateUrl: 'dashboard.component.html',
+  styleUrls: ['dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  books: string[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.books = ['Angular 2', 'AngularJS']
+  }
+
+}
