56c93292f2374b91c97da5487845170445601316
Gregor Woiwode <gw@co-it.eu>
2016-06-21 08:57:45 +0200

    Uses component dashboard in our app

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { DashboardComponent } from './dashboard';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  directives: [DashboardComponent]
 })
 export class AppComponent { }
