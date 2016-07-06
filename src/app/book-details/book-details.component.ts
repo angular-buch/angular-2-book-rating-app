ac38c4e85c8720fb2cccc2cfab15e7ba4dd51c2e
Gregor Woiwode <gw@co-it.eu>
2016-07-06 11:30:21 +0200

    Adds spike for model-driven forms

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES, FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, ROUTER_DIRECTIVES } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
-
 import { Book } from '../shared';
 import { BookStoreService } from '../services/book-store.service';
 
@@ -9,17 +9,36 @@ import { BookStoreService } from '../services/book-store.service';
   moduleId: module.id,
   selector: 'br-book-details',
   templateUrl: 'book-details.component.html',
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES, FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES]
 })
 export class BookDetailsComponent implements OnInit {
+  detailsForm: FormGroup;
   book: Book;
 
-  constructor(private route: ActivatedRoute, private bookStore: BookStoreService) {}
+  constructor(private route: ActivatedRoute,
+              private form: FormBuilder, 
+              private bookStore: BookStoreService) {}
 
   ngOnInit() {
+    this.getBook();
+    this.setupForm();
+  }
+
+  getBook() {
     this.route.params.subscribe(params => {
       let isbn = params['isbn'];
       this.bookStore.getSingle(isbn).subscribe(book => this.book = book);
     });
+  }
+
+  setupForm() {
+    this.detailsForm = this.form.group({
+      'title': '',
+      'description': ''
+    });
+  }
+
+  onSubmit(value: string): void {  
+    console.log('you submitted value: ', value);  
   }
 }
